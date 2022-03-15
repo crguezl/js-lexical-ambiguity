191931ff9fac98973d2c22f49da2f4606b5cc67d
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-03-15 16:56:35 +0000

    js-lexical-ambiguity

---
+++ hidden-amb.js
@@ -0,0 +1,4 @@
+let tutu = { map(_) { return 2}}
+let a = 5, b = 8, hi = 4, c = "hello", d =
+    g = { exec(_) { return tutu; }}
+module.exports = {a, b, hi, c, d, g}
---
+++ lexical-ambiguity.js
@@ -0,0 +1,4 @@
+let {a, b, hi, g, c, d} = require('./hidden-amb')
+a = b
+/hi/g.exec(c).map(d)
+console.log(a);
